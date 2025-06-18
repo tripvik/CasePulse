bd6768b95c0f930fdcebca017c29cec6d4f1be1b
Vikash Tripathi <tripvik@gmail.com>
2025-06-18 20:01:01 +0530

    new conversation detection

---
+++ SmartPendant.MAUIHybrid/SmartPendant.MAUIHybrid/Abstractions/IConversationService.cs
@@ -0,0 +1,79 @@
+﻿using SmartPendant.MAUIHybrid.Models;
+
+namespace SmartPendant.MAUIHybrid.Abstractions
+{
+    /// <summary>
+    /// Defines the contract for a service that handles the persistence
+    /// of conversation data and related entities.
+    /// </summary>
+    public interface IConversationService
+    {
+        #region Conversation Methods
+        /// <summary>
+        /// Saves a single conversation.
+        /// </summary>
+        /// <param name="conversation">The conversation object to save.</param>
+        Task SaveConversationAsync(Conversation conversation);
+
+        /// <summary>
+        /// Saves a collection of conversations.
+        /// </summary>
+        /// <param name="conversations">The collection of conversations to save.</param>
+        Task SaveConversationsAsync(IEnumerable<Conversation> conversations);
+
+        /// <summary>
+        /// Retrieves a single conversation by its unique ID.
+        /// </summary>
+        /// <param name="conversationId">The ID of the conversation to retrieve.</param>
+        /// <returns>The conversation object, or null if not found.</returns>
+        Task<Conversation?> GetConversationAsync(Guid conversationId);
+
+        /// <summary>
+        /// Retrieves all saved conversations.
+        /// </summary>
+        /// <returns>A list of all conversation objects.</returns>
+        Task<List<Conversation>> GetAllConversationsAsync();
+
+        /// <summary>
+        /// Retrieves all conversations recorded on a specific date.
+        /// </summary>
+        /// <param name="date">The date to filter conversations by.</param>
+        /// <returns>A list of conversations from the specified date.</returns>
+        Task<List<Conversation>> GetConversationsByDateAsync(DateTime date);
+
+        /// <summary>
+        /// Retrieves all conversations that contain a specific topic in their insights.
+        /// </summary>
+        /// <param name="topic">The topic to search for.</param>
+        /// <returns>A list of conversations related to the specified topic.</returns>
+        Task<List<Conversation>> GetConversationsByTopicAsync(string topic);
+
+        /// <summary>
+        /// Deletes a single conversation by its ID.
+        /// </summary>
+        /// <param name="conversationId">The ID of the conversation to delete.</param>
+        Task DeleteConversationAsync(Guid conversationId);
+
+        /// <summary>
+        /// Deletes multiple conversations by their IDs.
+        /// </summary>
+        /// <param name="conversationIds">The collection of conversation IDs to delete.</param>
+        Task DeleteConversationsAsync(IEnumerable<Guid> conversationIds);
+        #endregion
+
+        #region Task/ActionItem Methods
+        /// <summary>
+        /// Retrieves all action items from all conversations.
+        /// </summary>
+        /// <returns>A list of all action items.</returns>
+        Task<List<ActionItem>> GetAllTasksAsync();
+
+        /// <summary>
+        /// Retrieves all action items assigned to a specific person.
+        /// </summary>
+        /// <param name="assignee">The name of the assignee.</param>
+        /// <returns>A list of tasks assigned to the specified person.</returns>
+        Task<List<ActionItem>> GetTasksByAssigneeAsync(string assignee);
+        #endregion
+    }
+}
--- SmartPendant.MAUIHybrid/SmartPendant.MAUIHybrid/MauiProgram.cs
+++ SmartPendant.MAUIHybrid/SmartPendant.MAUIHybrid/MauiProgram.cs
@@ -65,8 +65,8 @@ namespace SmartPendant.MAUIHybrid
             builder.Services.AddBlazoredLocalStorage();
 
             // Register application-specific singleton services
-            builder.Services.AddSingleton<ConversationService>();
-            builder.Services.AddSingleton<AudioPipelineManager>();
+            builder.Services.AddScoped<IConversationService, LocalStorageConversationService>();
+            builder.Services.AddScoped<AudioPipelineManager>();
             builder.Services.AddScoped<UserPreferencesService>();
             builder.Services.AddScoped<LayoutService>();
             builder.Services.AddSingleton<IStorageService, BlobStorageService>();
